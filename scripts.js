bd0fb903b1352b6ecc175fe8ff70afe95fef8894
Rudolph Venter <rudolph.rv@gmail.com>
2019-11-14 16:26:00 +0200

    Initial Commit
    Basics done, no parallax or scanlines, yet.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var counter = 0;
+
+window.onload = function(){
+    //document.getElementById('main').innerHTML = "Hello, World!";
+};
+window.setInterval(function() {
+    //counter++;
+    //document.getElementById('main').innerHTML = counter;
+}, 1000);
