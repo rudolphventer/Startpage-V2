4b5e6001448e29b6cbf97c7bc5d7fa89d7cd607a
Rudolph Venter <rudolph.rv@gmail.com>
2019-11-16 02:15:00 +0200

    Adde asteroids in the background
    Also added some comments to make the code more readable.

--- scripts.js
+++ scripts.js
@@ -1,40 +1,46 @@
-window.onload = function(){
-//document.getElementById('main').innerHTML = "Hello, World!";
-};
+//variables
 var counter = false;
 var texttimer = 0;
+var x2 = 0;
+var y2 = 0;
+var floattimer = 0;
+
+//Zeroes searchbar on pageload
+window.onload = function(){
+this.document.getElementById("search").value = '';
+};
     
+//Controls mosue parallax
 document.addEventListener('mousemove', onMouseUpdate, false);
-
 function onMouseUpdate(e) {
-  var x = -(e.pageX + document.getElementById("landing-content").offsetLeft) / 10;
-  var y = -(e.pageY + document.getElementById("landing-content").offsetTop) / 10;
+  //The space background
+  var x = -(e.pageX + document.getElementById("landing-content").style.left) / 10;
+  var y = -(e.pageY + document.getElementById("landing-content").style.top) / 10;
   document.getElementById("landing-content").style.backgroundPosition = x + 'px ' + y + 'px';
-
-  //var x2 = ((e.pageX)-(window.innerWidth/2))/(window.innerWidth/2);
-  //console.log(x2);
-  //var y2 = ((e.pageY)-(window.innerHeight/2))/(window.innerHeight/2);
-  //document.getElementById("maincontent").style.transform ="rotateY("+ x2*30 +"deg)";
-  //document.getElementById("maincontent").style.transform ="rotateX("+ y2*30 +"deg)";
-  //document.getElementById("maincontent").style.marginLeft = y2 + 'px';
+  //The asteroids
+  x2 = -(e.pageX + document.getElementById("asteroids").offsetLeft) / 20;
+  y2 = -(e.pageY + document.getElementById("asteroids").offsetTop) / 20;
+  document.getElementById("asteroids").style.backgroundPosition = (x2 + floattimer/2) + 'px ' + y2 + 'px';
   }
 
+//timer 1, controls scanlines and changing text, half second intervals
 window.setInterval(function() {
+    //Scanlines
     if(counter == false)
     {
-        document.getElementById("scan").style.backgroundImage = 'url(scanline2.png)';
+        document.getElementById("scan").style.backgroundImage = 'url(media/scanline2.png)';
         counter = true;
     } else {
-        document.getElementById("scan").style.backgroundImage = 'url(scanline.png)';
+        document.getElementById("scan").style.backgroundImage = 'url(media/scanline.png)';
         counter = false;
     }
-
-    if(texttimer === 9)
+    //Changing text
+    if(texttimer === 5)
     {
-        document.getElementById("brandingtext").style.display = 'none';
-    } else if (texttimer >= 10)
+        document.getElementById("brandingtext").textContent = '';
+    } else if (texttimer >= 6)
     {
-        document.getElementById("brandingtext").style.display = 'flex';
+        document.getElementById("brandingtext").textContent = 'Hello traveler!';
         texttimer = 0;
     }
 
@@ -43,8 +49,19 @@ window.setInterval(function() {
 
 }, 500);
 
-////
+//timer 2, controls asteroids
+window.setInterval(function() {
+    floattimer++;
+    document.getElementById("asteroids").style.backgroundPosition = (x2 + floattimer/2) + 'px ' + y2 + 'px';
+    if(floattimer >= 1400)
+    {
+        floattimer = 0;
+    }
+}, 50);
 
+
+
+//searchbar
 var node = document.getElementsByClassName("search");
 document.addEventListener("keyup", function(event) {
     var searchstring =  document.getElementById("search").value; 
